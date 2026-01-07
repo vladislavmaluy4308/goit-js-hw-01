0314e2c90ade0d7b308e8b5fdf56f0f7af7b54e4
vladislavmaluy4308 <vlad.maluy666@gmail.com>
2026-01-07 21:29:48 +0200

    my first js

---
+++ js/task-3.js
@@ -0,0 +1,13 @@
+function getElementWidth(content, padding, border, Npx) {
+  const contentWidth = parseFloat(content);
+  console.log("🚀 ~ getElementWidth ~ contentWidth:", contentWidth);
+  const paddingWidth = parseFloat(padding);
+  console.log("🚀 ~ getElementWidth ~ paddingWidth:", paddingWidth);
+  const borderWidth = parseFloat(border);
+  console.log("🚀 ~ getElementWidth ~ borderWidth:", borderWidth);
+  return contentWidth + paddingWidth * 2 + borderWidth * 2;
+}
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
